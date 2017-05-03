ff5765ff11568a61d8d4b71f6f9ea51ea1efdd1f
Kirill Mayboroda <kirill.mayboroda@ncpr.su>
2017-05-04 00:55:57 +0300

    Alfresco context & models added as resources

--- src/main/java/ru/itdsystems/aconfgen/plugin/AbstractAlfrescoConfMojo.java
+++ src/main/java/ru/itdsystems/aconfgen/plugin/AbstractAlfrescoConfMojo.java
@@ -1,14 +1,6 @@
 package ru.itdsystems.aconfgen.plugin;
 
-import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Kirill Mayboroda on 24.04.17.
--- src/main/java/ru/itdsystems/aconfgen/plugin/GenerateConstantsMojo.java
+++ src/main/java/ru/itdsystems/aconfgen/plugin/GenerateConstantsMojo.java
@@ -1,18 +1,21 @@
 package ru.itdsystems.aconfgen.plugin;
 
-import org.alfresco.repo.dictionary.M2Namespace;
-import org.alfresco.repo.dictionary.M2Property;
-import org.alfresco.repo.dictionary.M2Type;
+import org.alfresco.service.cmr.dictionary.TypeDefinition;
+import org.alfresco.repo.dictionary.*;
+import org.alfresco.service.namespace.QName;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import org.alfresco.repo.dictionary.M2Model;
-
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Collection;
 import java.util.List;
 
 /**
@@ -21,10 +24,20 @@ import java.util.List;
 @Mojo(name = "gen-const", defaultPhase = LifecyclePhase.POST_CLEAN, threadSafe = true)
 public class GenerateConstantsMojo extends AbstractMojo {
 
+    ApplicationContext context;
+
     @Parameter
     private String model;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
+        // Could not get context from alfresco jars.
+        // Required contexts:
+        // org.alfresco.alfresco-data-model-${alfresco.version}.jar/alfresco/data-model-stand-alone-context.xml
+        // org.alfresco.alfresco-repository-${alfresco.version}.jar/alfresco/core-services-context.xml
+        context = new ClassPathXmlApplicationContext("data-model-context.xml");
+        DictionaryDAO dictionaryDAO = (DictionaryDAO) context.getBean("dictionaryDAO");
+        NamespaceDAO namespaceDAO = (NamespaceDAO) context.getBean("namespaceDAO");
+
         InputStream inputStream = null;
         try {
             inputStream = new FileInputStream(model);
@@ -36,6 +49,13 @@ public class GenerateConstantsMojo extends AbstractMojo {
         for (M2Type type : types) {
             for (M2Property property : type.getProperties()) {
                 getLog().info(model.getNamespaces().get(0).getUri() + " " + property.getName());
+            }
+        }
+        CompiledModel compiledModel = model.compile(dictionaryDAO, namespaceDAO, true);
+        Collection<TypeDefinition> typeDefinitions = compiledModel.getTypes();
+        for (TypeDefinition typeDefinition : typeDefinitions) {
+            for (QName qName : typeDefinition.getProperties().keySet()) {
+                getLog().info(qName.toString());
             }
         }
     }
